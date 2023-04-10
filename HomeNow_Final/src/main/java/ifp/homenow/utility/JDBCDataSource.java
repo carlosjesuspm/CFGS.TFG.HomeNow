80922582bfd31a86d15cffebfcd2d97822e1b37c
Carlos Jesús Pérez Márquez <permarcar96@gmail.com>
2023-04-10 23:59:58 +0200

    JDBCDataSource y conexión correcta a BBDD homenow

--- HomeNow_Final/src/main/java/ifp/homenow/utility/JDBCDataSource.java
+++ HomeNow_Final/src/main/java/ifp/homenow/utility/JDBCDataSource.java
@@ -1,7 +1,8 @@
 package ifp.homenow.utility;
 
+import java.sql.Connection;
 import java.util.ResourceBundle;
-
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 /*Interface que hereda de CommonDataSource y WrapperClass. Contiene dos métodos como 
  * getConnection() y getConnection(String username, String password). Se emplea como 
@@ -9,12 +10,66 @@ import java.util.ResourceBundle;
  */
 
 public class JDBCDataSource {
+	/**
+	 * JDBC Database connection pool ( DCP )
+	 */
+	private static JDBCDataSource datasource;
 
-	//Orientación hacia archivo properties para configuración de los datos de la BBDD
-	ResourceBundle rb=ResourceBundle.getBundle("homeNowApp.System");
-	
-	//Obtener url de la BBDD
-	String url = rb.getString("url");
-	
-	
+	private JDBCDataSource() {
+	}
+
+	private ComboPooledDataSource cpds = null;
+
+	/**
+	 * Create instance of Connection Pool
+	 *
+	 * @return
+	 */
+	public static JDBCDataSource getInstance() {
+		if (datasource == null) {
+			ResourceBundle rb = ResourceBundle.getBundle("homeNowApp.System");
+			datasource = new JDBCDataSource();
+			datasource.cpds = new ComboPooledDataSource();
+			try {
+				datasource.cpds.setDriverClass(rb.getString("driver"));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			datasource.cpds.setJdbcUrl(rb.getString("url"));
+			datasource.cpds.setUser(rb.getString("username"));
+			datasource.cpds.setPassword(rb.getString("password"));
+			datasource.cpds.setInitialPoolSize(Integer.valueOf((String) rb.getString("initialPoolSize")));
+			datasource.cpds.setAcquireIncrement(Integer.valueOf((String) rb.getString("acquireIncrement")));
+			datasource.cpds.setMaxPoolSize(Integer.valueOf((String) rb.getString("maxPoolSize")));
+			datasource.cpds.setMaxIdleTime(DataUtility.getInt(rb.getString("timeout")));
+			datasource.cpds.setMinPoolSize(Integer.valueOf((String) rb.getString("minPoolSize")));
+		}
+		return datasource;
+	}
+
+	/**
+	 * Gets the connection from ComboPooledDataSource
+	 *
+	 * @return connection
+	 */
+	public static Connection getConnection() throws Exception {
+		return getInstance().cpds.getConnection();
+	}
+
+	/**
+	 * Closes a connection4
+	 *
+	 * @param connection
+	 * @throws Exception
+	 */
+	public static void closeConnection(Connection connection) {
+		if (connection != null) {
+			try {
+				connection.close();
+			} catch (Exception e) {
+			}
+		}
+	}
+		
+
 }
