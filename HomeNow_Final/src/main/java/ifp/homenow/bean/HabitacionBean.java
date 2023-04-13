f74f2711548579d27f47f3b7fc239c1605e7faed
Carlos Jesús Pérez Márquez <permarcar96@gmail.com>
2023-04-13 08:17:08 +0200

    HabitaciónBean

---
+++ HomeNow_Final/src/main/java/ifp/homenow/bean/HabitacionBean.java
@@ -0,0 +1,60 @@
+package ifp.homenow.bean;
+
+public class HabitacionBean {
+
+	//Atributos
+	private long idhabitacion;
+	private int precio_hab;
+	private DireccionBean direccion_hab;
+	private int companeros;
+	private String imagenes_hab;
+	private String descripcion_hab;
+	private UsuarioBean usuarios_hab;
+	
+	
+	//Getters y Setters
+	public long getIdhabitacion() {
+		return idhabitacion;
+	}
+	public void setIdhabitacion(long idhabitacion) {
+		this.idhabitacion = idhabitacion;
+	}
+	public int getPrecio_hab() {
+		return precio_hab;
+	}
+	public void setPrecio_hab(int precio_hab) {
+		this.precio_hab = precio_hab;
+	}
+	public DireccionBean getDireccion_hab() {
+		return direccion_hab;
+	}
+	public void setDireccion_hab(DireccionBean direccion_hab) {
+		this.direccion_hab = direccion_hab;
+	}
+	public int getCompaneros() {
+		return companeros;
+	}
+	public void setCompaneros(int companeros) {
+		this.companeros = companeros;
+	}
+	public String getImagenes_hab() {
+		return imagenes_hab;
+	}
+	public void setImagenes_hab(String imagenes_hab) {
+		this.imagenes_hab = imagenes_hab;
+	}
+	public String getDescripcion_hab() {
+		return descripcion_hab;
+	}
+	public void setDescripcion_hab(String descripcion_hab) {
+		this.descripcion_hab = descripcion_hab;
+	}
+	public UsuarioBean getUsuarios_hab() {
+		return usuarios_hab;
+	}
+	public void setUsuarios_hab(UsuarioBean usuarios_hab) {
+		this.usuarios_hab = usuarios_hab;
+	}
+	
+	
+}
