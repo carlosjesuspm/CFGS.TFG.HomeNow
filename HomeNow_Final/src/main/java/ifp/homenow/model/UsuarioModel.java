6a60e49a9578e6755af291deabd04d8d5533957c
Carlos Jesús Pérez Márquez <permarcar96@gmail.com>
2023-04-13 08:42:59 +0200

    UsuarioModel

--- HomeNow_Final/src/main/java/ifp/homenow/model/UsuarioModel.java
+++ HomeNow_Final/src/main/java/ifp/homenow/model/UsuarioModel.java
@@ -13,44 +13,47 @@ import ifp.homenow.utility.JDBCDataSource;
 public class UsuarioModel {
 
 	public static long nextPk() {
-	    long pk = 0;
-	    Connection conn;
-	    try {
-	      conn = JDBCDataSource.getConnection();
-	      PreparedStatement stmt = conn.prepareStatement("select Max(id) from user");
-	      ResultSet rs = stmt.executeQuery();
-	      while(rs.next()){
-	        pk = rs.getLong(1);
-	      }
-	    } catch (Exception e) {
-	      // TODO Auto-generated catch block
-	      e.printStackTrace();
-	    }
-	    return pk+1;
-	    
-	  }
-	  
-	  
-	  public static long addUser(UsuarioBean user) {
-	    int i = 0;
-	    try {
-	      Connection conn = JDBCDataSource.getConnection();
-	      PreparedStatement stmt = conn.prepareStatement("insert into user values(?,?,?,?,?,?,?)");
-	      stmt.setLong(1, nextPk());
-	      stmt.setString(2 , user.getNombre() );
-	      stmt.setString(3 , user.getApellido1() );
-	      stmt.setString(4 , user.getApellido2() );
-	      stmt.setString(5 , user.getEdad() );
-	      stmt.setDate(6 , new java.sql.Date(user.getDob().getTime()) );
-	      stmt.setString(7 , user.getMobileNo() );
-	        i =     stmt.executeUpdate();
-	      
-	    } catch (Exception e) {
-	      // TODO Auto-generated catch block
-	      e.printStackTrace();
-	    }
-	    
-	    
-	    return i;
-	  }
+		long pk = 0;
+		Connection conn;
+		try {
+			conn = JDBCDataSource.getConnection();
+			PreparedStatement stmt = conn.prepareStatement("select Max(id) from user");
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {
+				pk = rs.getLong(1);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return pk + 1;
+
+	}
+
+	public static long addUser(UsuarioBean user) {
+		int i = 0;
+		try {
+			
+			Connection conn = JDBCDataSource.getConnection();
+			PreparedStatement stmt = conn.prepareStatement("insert into user values(?,?,?,?,?,?,?,?,?,?)");
+			stmt.setLong(1, nextPk());
+			stmt.setString(2, user.getNombre());
+			stmt.setString(3, user.getApellido1());
+			stmt.setString(4, user.getApellido2());
+			stmt.setDate(5, new java.sql.Date(user.getFecha_nacimiento().getTime()));
+			stmt.setInt(6, user.getEdad());
+			stmt.setString(7, user.getUsuario());
+			stmt.setString(8, user.getContrasena());
+			stmt.setInt(9, user.getTelefono());
+			stmt.setString(10, user.getCorreo());
+
+			i = stmt.executeUpdate();
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return i;
+	}
 }
